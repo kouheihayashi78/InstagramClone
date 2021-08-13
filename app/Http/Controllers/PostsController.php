096562dba11c409a5051e2e246722264455fa37a
kouheihayashi78 <kouhei.basketball7.8@gmail.com>
2021-08-13 15:38:24 +0900

    プロフィール編集機能追加

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 use App\Models\User;
+use Auth;
 use Illuminate\Http\Request;
+use Validator;
 
 class PostsController extends Controller
 {
@@ -16,6 +18,36 @@ class PostsController extends Controller
     {
         $user = User::where('id', $user_id)->first();
         return view('user/show', compact('user'));
+    }
+
+    public function edit()
+    {
+        $user = Auth::user();
+
+        return view('user/edit', compact('user'));
+    }
+
+    public function update(Request $request)
+    {
+        $validate = $request->validate([
+            'user_name' => 'required|string|max:255',
+            'user_password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if($validate->fails())
+        {
+            return redirect()->back()->withErrors($validate->errors())->withInput();
+        }
+        $user = User::find($request->id);
+        $user->name = $request->user_name;
+        if($request->user_profile_photo !=null)
+        {
+            $request->user_profile_photo->storeAs('public/user_images', $user->id. '.jpg');
+            $user->profile_photo = $user->id. '.jpg';
+        }
+        $user->password = bcrypt($request->user_password);
+        $user->save();
+        return redirect('/users/', $request->id);
     }
 
 }
--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -17,11 +17,28 @@
         <div class="col-md-8">
             <div class="row">
                 <h1>{{ $user->name }}</h1>
-
-                <a class="btn btn-outline-dark common-btn edit-profile-btn" href="/users/edit">プロフィールを編集</a>
+                @if ($user->id == Auth::user()->id)
+                <a class="btn btn-outline-dark common-btn edit-profile-btn" href="/edit">プロフィールを編集</a>
                 <a class="btn btn-outline-dark common-btn edit-profile-btn" rel="nofollow" data-method="POST" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ログアウト</a>
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
-
+                @endif
+                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">設定</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="list-group text-center">
+                                <a class="list-group-item list-group-item-action" rel="nofollow" data-method="POST" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ログアウト</a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
+                                <a class="list-group-item list-group-item-action" data-dismiss="modal" href="#">キャンセル</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="row">
                 <p>
